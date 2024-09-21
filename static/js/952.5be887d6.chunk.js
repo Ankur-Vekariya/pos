17a37d088b2fe2deebb89c847996e2199775dd35
Ankur-Vekariya <ankurvekariya2001@gmail.com>
2024-09-21 18:55:43 +0530

    Updates

---
+++ static/js/952.5be887d6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpos=self.webpackChunkpos||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.5be887d6.chunk.js.map
